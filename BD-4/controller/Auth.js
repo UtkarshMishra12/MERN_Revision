467a64adb88e1d11c1d294d2e1fa6d166b84ad6c
Utkarsh Mishra <nasautkarsh@gmail.com>
2025-03-30 12:09:06 +0200

    JWT Added
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- BD-4/controller/Auth.js
+++ BD-4/controller/Auth.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 const bcrypt = require("bcrypt");
 
@@ -16,13 +18,12 @@ const signUp = async (req, res) => {
     }
 
     let hashedPassword;
-    try{
-        hashedPassword = await bcrypt.hash(password, 10);
-    }
-    catch(error){
-        return res.status(400).json({
-            message: error.message
-        });
+    try {
+      hashedPassword = await bcrypt.hash(password, 10);
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
     }
 
     const user = new User({
@@ -37,48 +38,72 @@ const signUp = async (req, res) => {
     return res.status(201).json({
       message: "User created successfully",
     });
-
-  } 
-  
-  catch (error) {
+  } catch (error) {
     return res.status(400).json({
       message: error.message,
     });
   }
 };
- 
+
 const signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter email and password",
+      });
+    }
 
     const user = await User.findOne({ email });
 
     if (!user) {
       return res.status(400).json({
-        message: "Invalid email or password",
+        success: false,
+        message: "User does not exist",
       });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
+    const payload = {
+      id: user._id,
+      email: user.email,
+      role: user.role,
+    };
+
+    if (isMatch) {
+      let token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      usrer.token = token;
+      user.password = undefined;
+
+      
+
+      return res.status(200).json({
+        success: true,
+        message: "User signed in successfully",
+        token,
+      });
+
+    } 
+    else {
       return res.status(400).json({
-        message: "Invalid email or password",
+        message: "Invalid password",
       });
     }
 
     return res.status(200).json({
       message: "User signed in successfully",
     });
-
-  } 
-  
-  catch (error) {
+  } catch (error) {
     return res.status(400).json({
       message: error.message,
     });
   }
-}
-
+};
 
 module.exports = { signUp, signIn };
