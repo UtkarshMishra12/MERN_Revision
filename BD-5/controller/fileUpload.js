745a7a4dc60fdd76c08ac5b1883f15567466a086
Utkarsh Mishra <nasautkarsh@gmail.com>
2025-04-01 23:11:11 +0200

    Image Size reduce upload
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- BD-5/controller/fileUpload.js
+++ BD-5/controller/fileUpload.js
@@ -30,8 +30,11 @@ function isFileFormatSupported(fileFormat, supportedFormats) {
     return supportedFormats.includes(fileFormat);
 }
 
-async function uploadFileToCloudinary(file, folder){
+async function uploadFileToCloudinary(file, folder, quality){
     const options = {folder};
+    if(quality){
+        options.quality = quality;
+    }
     options.resource_type = "auto";
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
@@ -117,5 +120,48 @@ exports.videoUpload = async (req,res) =>{
     catch(error){
         console.log(error);
         res.status(500).json({message: "Internal Server Error while uploading video"});
+    }
+}
+
+exports.imageReducerUpload = async(req,res)=>{
+    try{
+        const {name,tag,email} = req.body;
+        console.log(name,tag,email);
+
+        const file = req.files.imageFile;
+        console.log(file);
+
+        const supportedFormats = ["jpg", "jpeg", "png", "gif"];
+        const fileFormat = file.name.split(".")[1].toLowerCase();
+       
+        if(!isFileFormatSupported(fileFormat, supportedFormats)){
+            return res.status(400).json({
+                message: "Unsupported file format",
+                success: false,
+            });
+        }
+        //file format supported 
+        const response = await uploadFileToCloudinary(file, "Mishra_Utkarsh_April", 30);
+        console.log(response);
+        //save to db
+        const fileData = await File.create({
+            name,
+            email,
+            tags: tag,
+            imageUrl: response.secure_url,
+        });
+        
+
+        res.status(200).json({
+            message: "File Uploaded Successfully",
+            success: true,
+            imageUrl: response.secure_url,
+        });
+
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message: "Internal Server Error while uploading image with reduced size"});
     }
 }
--- BD-5/routes/FileUpload.js
+++ BD-5/routes/FileUpload.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {localFileUpload, imageUpload, videoUpload } = require('../controller/fileUpload');
-//mageUpload, videoUpload, imageReducerUpload,
+const {localFileUpload, imageUpload, videoUpload, imageReducerUpload} = require('../controller/fileUpload');
+
 
 router.post('/imageUpload', imageUpload);
 router.post('/videoUpload', videoUpload);
-// router.post('/imageReducer', imageReducerUpload);
+router.post('/imageReducer', imageReducerUpload);
 router.post('/localFileUpload', localFileUpload);
 
 module.exports = router;
