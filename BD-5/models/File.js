9149e6e70d53e417d38027ea8aa83c990e20e8e5
Utkarsh Mishra <nasautkarsh@gmail.com>
2025-04-06 20:05:15 +0200

    Sending Email after uploading the Assets
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- BD-5/models/File.js
+++ BD-5/models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema(
     {
@@ -16,5 +18,33 @@ const fileSchema = new mongoose.Schema(
         type: String,
     }
 });
+
+//post nodemailer
+fileSchema.post("save", async function (doc) {
+    try{
+        console.log("File saved successfully:", doc);
+
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        //send mail
+        let info =await transporter.sendMail({
+            from: `Utkarsh Mishra`,
+            to: doc.email,
+            subject: "File Upload Confirmation",
+            text: ` <h1> Hello from Utkarsh </h1> Your file ${doc.name} has been uploaded successfully.`,
+        });
+
+        console.log(info);
+    }
+    catch(err){
+        console.log(err);
+    }
+})
 
 module.exports = mongoose.model("File", fileSchema);
